453c7296ff4c26ecde0971bb62c26b7721d6d534
Christopher Kuzma <chris.kuzma@gmail.com>
2017-02-16 01:32:13 -0500

    Updated with some pretty printing of recognized values

--- Python Code/serial_read.py
+++ Python Code/serial_read.py
@@ -1,5 +1,9 @@
+import json
 import os
 import serial
+
+raw_print = False
+pretty_print = True
 
 with serial.Serial('COM3', timeout=3.0) as ser:
     while True:
@@ -7,7 +11,13 @@ with serial.Serial('COM3', timeout=3.0) as ser:
         data = data.split(',')
         os.system('cls')
         known_readings = {
-            "temp": data[6]
+            "temp": data[6],
+            "amb_light": data[8]
         }
-        for point in data:
-            print(point)
+
+        if pretty_print is True:
+            print(json.dumps(known_readings, indent=2))
+
+        if raw_print is True:
+            for point in data:
+                print(point)
